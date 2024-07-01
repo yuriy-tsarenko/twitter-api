ca1c5c6a0499df93a9e5a9bc9d2400c2143981a7
Yuriy Tsarenko <tsarenkoyuriidev@gmail.com>
2024-07-01 20:09:42 +0300

    init

---
+++ src/main/groovy/com/twitter/twitterapi/TwitterApiApplication.groovy
@@ -0,0 +1,13 @@
+package com.twitter.twitterapi
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TwitterApiApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TwitterApiApplication, args)
+    }
+
+}
