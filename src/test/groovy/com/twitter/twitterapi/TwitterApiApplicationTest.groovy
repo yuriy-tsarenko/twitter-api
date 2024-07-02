3bef10fefe5a421b9b2fef1a1d251b3490954dbd
Yuriy Tsarenko <tsarenkoyuriidev@gmail.com>
2024-07-02 12:40:00 +0300

    #TWIT-002: switch to spock

--- src/test/groovy/com/twitter/twitterapi/TwitterApiApplicationTest.groovy
+++ src/test/groovy/com/twitter/twitterapi/TwitterApiApplicationTest.groovy
@@ -1,16 +1,17 @@
 package com.twitter.twitterapi
 
 import com.twitter.twitterapi.config.MongoTestConfiguration
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import spock.lang.Specification
 
 @ActiveProfiles("test")
 @SpringBootTest(classes = [MongoTestConfiguration])
-class TwitterApiApplicationTests {
+class TwitterApiApplicationTest extends Specification {
 
-    @Test
-    void contextLoads() {
+    def "context loads"() {
+        expect:
+        true
     }
 
 }
